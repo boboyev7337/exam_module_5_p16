f10a9bdc1cfc0a6530e7b61f18b9df5058f342a3
boboyev7337 <bekov7337@gmail.com>
2023-11-25 21:18:35 +0500

    Last

--- bot.py
+++ bot.py
@@ -1,21 +1,16 @@
 import asyncio
 import logging
-import os
 import sys
-from os import getenv
-
+import os
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 from dotenv import load_dotenv
-from db import Session,engine,User,Message
-
+from db import Session, User,Message
 load_dotenv('.env')
-
 # Bot token can be obtained via https://t.me/BotFather
-TOKEN = getenv("BOT_TOKEN")
 
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
@@ -23,25 +18,29 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    user_id = str(message.from_user.id)
+    user_telegram_id = str(message.from_user.id)
     username = message.from_user.username
-    first_name = message.from_user.first_name
-    created_at = str(message.date)
-    user = User(user_id=user_id,username=username,first_name=first_name,created_at=created_at)
+    created = str(message.date)
+    user = User(user_telegram_id=user_telegram_id,username=username,created=created)
     session = Session()
     session.add(user)
     session.commit()
     await message.reply("Ma`lumot saqlandi")
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
+@dp.message()
+async def messages_from_users_handler(message: types.Message):
+    text = str(message.text)
+    created = str(message.date)
+    Mess = Message(text=text,created=created)
+    session = Session()
+    session.add(Mess)
+    session.commit()
+    await message.reply("Habar saqlandi")
 
 @dp.message()
 async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
 
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
     try:
         # Send a copy of the received message
         await message.send_copy(chat_id=message.chat.id)
--- db.py
+++ db.py
@@ -1,30 +1,33 @@
 import os
-
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.orm import sessionmaker, declarative_base
+from os import getenv
+from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 load_dotenv('.env')
+# from config import DATABASE_URL
 
-engine = create_engine(os.getenv(os.getenv('DATABASE_URL')), echo=True)
+engine = create_engine(os.getenv("DATABASE_URL"), echo=True)
 
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
-    id = Column(Integer,primary_key=True)
-    user_telegram_id = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    user_telegram_id = Column(String)
     username = Column(String)
-    created = Column(DateTime)
+    created = Column(String)
+
 
 class Message(Base):
-    __tablename__ = 'messages'
-
-    id = Column(Integer,primary_key=True)
-    user_id = Column(String)
+    __tablename__ = "messages"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
     text = Column(String)
-    created = Column(DateTime)
+    created =Column(String)
+
 
 Base.metadata.create_all(engine)
